ca726a3c8133301e3d9922dcba907cc657935a4d
Abdullah272056 <abdullah272056@gmail.com>
2021-01-02 12:19:29 +0600

    register page design

--- app/src/main/java/com/example/hostelmealmanagement/MainActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/MainActivity.java
@@ -10,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //token
+        //eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVmZWIxODlmYTA5OGRlMDAxNzNhNjY3YiIsImlhdCI6MTYwOTMxNDkxNCwiZXhwIjoxNjExOTA2OTE0fQ.2N-4r1UEczEpqIXT6kYp17CzC8VoasBzgy_pyw9tklI
     }
 }
