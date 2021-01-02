b78d3efb9e59e9a5bd73cef311c10183697919ee
Abdullah272056 <abdullah272056@gmail.com>
2021-01-02 13:14:55 +0600

    create interface for registration

--- app/src/main/java/com/example/hostelmealmanagement/retrofit/ApiInterface.java
+++ app/src/main/java/com/example/hostelmealmanagement/retrofit/ApiInterface.java
@@ -1,5 +1,16 @@
 package com.example.hostelmealmanagement.retrofit;
 
+import com.example.hostelmealmanagement.retrofit.register.RegisterGetDataResponse;
+import com.example.hostelmealmanagement.retrofit.register.RegisterSetDataResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
 public interface ApiInterface {
+    //registration
+    @POST("api/auth/register")
+    Call<RegisterGetDataResponse> registrationData(@Body RegisterSetDataResponse registerSetDataResponse);
+
 
 }
