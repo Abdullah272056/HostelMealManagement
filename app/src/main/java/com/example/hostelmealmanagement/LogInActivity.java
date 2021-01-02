30b65b0e0c84622574af8aa791f645ef96ed56a6
Abdullah272056 <abdullah272056@gmail.com>
2021-01-02 16:30:22 +0600

    view finding

--- app/src/main/java/com/example/hostelmealmanagement/LogInActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/LogInActivity.java
@@ -3,13 +3,25 @@ package com.example.hostelmealmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+    EditText emailEditText,passwordEditText;
+    Button signInButton;
+    CheckBox rememberCheckBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+        //view finding
+        emailEditText=findViewById(R.id.signInEmailEditTextId);
+        passwordEditText=findViewById(R.id.signInPasswordEditTextId);
+        signInButton=findViewById(R.id.signInButtonId);
+        rememberCheckBox=findViewById(R.id.rememberCheckBoxId);
+
 
     }
 }
