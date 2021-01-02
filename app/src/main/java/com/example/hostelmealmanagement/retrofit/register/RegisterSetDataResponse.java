8f21cffe63795551e1db68d3debc822fbb82217d
Abdullah272056 <abdullah272056@gmail.com>
2021-01-02 12:50:02 +0600

    create RegisterSetDataResponse.java and add code

---
+++ app/src/main/java/com/example/hostelmealmanagement/retrofit/register/RegisterSetDataResponse.java
@@ -0,0 +1,70 @@
+package com.example.hostelmealmanagement.retrofit.register;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class RegisterSetDataResponse {
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("hostelName")
+    @Expose
+    private String hostelName;
+    @SerializedName("role")
+    @Expose
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getHostelName() {
+        return hostelName;
+    }
+
+    public void setHostelName(String hostelName) {
+        this.hostelName = hostelName;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public RegisterSetDataResponse(String name, String email, String password, String hostelName, String role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.hostelName = hostelName;
+        this.role = role;
+    }
+}
