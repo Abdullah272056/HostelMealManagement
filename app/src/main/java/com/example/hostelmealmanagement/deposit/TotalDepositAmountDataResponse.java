29d496cc4b3889cbb1623823356b993fd2174092
Abdullah272056 <abdullah272056@gmail.com>
2021-01-03 21:00:30 +0600

    write code in TotalDepositAmountDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/deposit/TotalDepositAmountDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/deposit/TotalDepositAmountDataResponse.java
@@ -1,5 +1,29 @@
 package com.example.hostelmealmanagement.deposit;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class TotalDepositAmountDataResponse {
-    
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private Integer data;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Integer getData() {
+        return data;
+    }
+
+    public void setData(Integer data) {
+        this.data = data;
+    }
 }
