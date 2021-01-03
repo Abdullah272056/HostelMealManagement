78c2cea1e08baf647390f944cba2d912980f13a7
Abdullah272056 <abdullah272056@gmail.com>
2021-01-04 00:20:32 +0600

    button finding and set on click listener

--- app/src/main/java/com/example/hostelmealmanagement/ExpensesActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/ExpensesActivity.java
@@ -3,12 +3,22 @@ package com.example.hostelmealmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ExpensesActivity extends AppCompatActivity {
-
+FloatingActionButton addExpenseButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
+        addExpenseButton=findViewById(R.id.addExpenseButtonId);
+        addExpenseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
