befee2e0b89b00246871a53acb6b3694969e00cf
Abdullah272056 <abdullah272056@gmail.com>
2021-01-03 20:51:11 +0600

    view finding

--- app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
@@ -3,12 +3,19 @@ package com.example.hostelmealmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    TextView totalDepositTextView,totalExpenseTextView,totalMealTextView,mealRateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //view finding
+        totalDepositTextView=findViewById(R.id.totalDepositTextViewId);
+        totalExpenseTextView=findViewById(R.id.totalExpenseTextViewId);
+        totalMealTextView=findViewById(R.id.totalMealTextViewId);
+        mealRateTextView=findViewById(R.id.mealRateTextViewId);
     }
 }
