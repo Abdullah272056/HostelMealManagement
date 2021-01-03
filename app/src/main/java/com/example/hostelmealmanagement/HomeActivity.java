47b517b06ceecf00ef1488303fbf7fa21a84ea67
Abdullah272056 <abdullah272056@gmail.com>
2021-01-03 21:27:47 +0600

    create totalDepositAmount method and call in onCreate

--- app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
@@ -4,9 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.hostelmealmanagement.deposit.TotalDepositAmountDataResponse;
 import com.example.hostelmealmanagement.retrofit.ApiInterface;
 import com.example.hostelmealmanagement.retrofit.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeActivity extends AppCompatActivity {
     TextView totalDepositTextView,totalExpenseTextView,totalMealTextView,mealRateTextView;
@@ -27,7 +33,30 @@ public class HomeActivity extends AppCompatActivity {
         totalExpenseTextView=findViewById(R.id.totalExpenseTextViewId);
         totalMealTextView=findViewById(R.id.totalMealTextViewId);
         mealRateTextView=findViewById(R.id.mealRateTextViewId);
+        totalDepositAmount();
     }
 
+    public void totalDepositAmount(){
+        apiInterface.totalDepositAmount("Bearer "+token).enqueue(new Callback<TotalDepositAmountDataResponse>() {
+            @Override
+            public void onResponse(Call<TotalDepositAmountDataResponse> call, Response<TotalDepositAmountDataResponse> response) {
+                if (response.code()==200){
+                    totalDepositTextView.setText("$"+String.valueOf(response.body().getData()));
+                }
+                else if (response.code()==401){
+                    Toast.makeText(HomeActivity.this, "Not Authorized to access this route", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(HomeActivity.this, "failed", Toast.LENGTH_SHORT).show();
 
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TotalDepositAmountDataResponse> call, Throwable t) {
+                Toast.makeText(HomeActivity.this, "failed", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+    }
 }
