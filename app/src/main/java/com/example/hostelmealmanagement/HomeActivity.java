57faa0f5f6327b3d3779551615b5828293d01430
Abdullah272056 <abdullah272056@gmail.com>
2021-01-03 21:15:02 +0600

    initialize apiInterface

--- app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
@@ -5,20 +5,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.hostelmealmanagement.retrofit.ApiInterface;
+import com.example.hostelmealmanagement.retrofit.RetrofitClient;
+
 public class HomeActivity extends AppCompatActivity {
     TextView totalDepositTextView,totalExpenseTextView,totalMealTextView,mealRateTextView;
     String token;
+
+    ApiInterface apiInterface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         //receive user token
         token= getIntent().getStringExtra("token");
-        
+        //initialize apiInterface
+        apiInterface = RetrofitClient.getRetrofit("http://hostel-meal-calc.herokuapp.com/").create(ApiInterface.class);
+
         //view finding
         totalDepositTextView=findViewById(R.id.totalDepositTextViewId);
         totalExpenseTextView=findViewById(R.id.totalExpenseTextViewId);
         totalMealTextView=findViewById(R.id.totalMealTextViewId);
         mealRateTextView=findViewById(R.id.mealRateTextViewId);
     }
+
+
 }
