bace8c94d4bcf2519fc70ab8824bd813e3ec0543
Abdullah272056 <abdullah272056@gmail.com>
2021-01-03 21:13:29 +0600

    receive token in HomeActivity.java

--- app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/HomeActivity.java
@@ -7,12 +7,14 @@ import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
     TextView totalDepositTextView,totalExpenseTextView,totalMealTextView,mealRateTextView;
-
+    String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+        //receive user token
+        token= getIntent().getStringExtra("token");
+        
         //view finding
         totalDepositTextView=findViewById(R.id.totalDepositTextViewId);
         totalExpenseTextView=findViewById(R.id.totalExpenseTextViewId);
