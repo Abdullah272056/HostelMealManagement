4c76ca64efd4415f0c6cf40dbf25d4fbd3a0462e
Abdullah272056 <abdullah272056@gmail.com>
2021-01-04 21:21:06 +0600

    customAdapter complete

--- app/src/main/java/com/example/hostelmealmanagement/get_all_member/MemberListCustomAdapter.java
+++ app/src/main/java/com/example/hostelmealmanagement/get_all_member/MemberListCustomAdapter.java
@@ -1,12 +1,16 @@
 package com.example.hostelmealmanagement.get_all_member;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.hostelmealmanagement.R;
+import com.example.hostelmealmanagement.expense.GetAllExpenseCustomAdapter;
 import com.example.hostelmealmanagement.expense.GetAllExpenseData;
 
 import java.util.List;
@@ -26,22 +30,25 @@ public class MemberListCustomAdapter extends RecyclerView.Adapter<MemberListCust
     @NonNull
     @Override
     public MemberListCustomAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.member_list_item,parent,false);
+        return new MemberListCustomAdapter.MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MemberListCustomAdapter.MyViewHolder holder, int position) {
-
+        holder.marketerTextView.setText(getAllMemberDataList.get(position).getName());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return getAllMemberDataList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView marketerTextView;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            marketerTextView=itemView.findViewById(R.id.marketerNameTextViewId);
         }
     }
 }
