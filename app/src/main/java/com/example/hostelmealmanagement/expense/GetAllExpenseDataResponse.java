a87c7c57d43cef4afcd2c5ec9db512e79433e51d
Abdullah272056 <abdullah272056@gmail.com>
2021-01-04 13:57:53 +0600

    add code in GetAllExpenseDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/expense/GetAllExpenseDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/expense/GetAllExpenseDataResponse.java
@@ -1,5 +1,31 @@
 package com.example.hostelmealmanagement.expense;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class GetAllExpenseDataResponse {
-    
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private List<GetAllExpenseData> getAllExpenseDataList = null;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<GetAllExpenseData> getGetAllExpenseDataList() {
+        return getAllExpenseDataList;
+    }
+
+    public void setGetAllExpenseDataList(List<GetAllExpenseData> getAllExpenseDataList) {
+        this.getAllExpenseDataList = getAllExpenseDataList;
+    }
 }
