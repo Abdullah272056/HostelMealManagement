a822f2c26cf979ad786673c507865fae3c8c9163
Abdullah272056 <abdullah272056@gmail.com>
2021-01-04 15:06:38 +0600

    create GetAllExpenseCustomAdapter.java and extent Recyclerview adapter

---
+++ app/src/main/java/com/example/hostelmealmanagement/expense/GetAllExpenseCustomAdapter.java
@@ -0,0 +1,31 @@
+package com.example.hostelmealmanagement.expense;
+
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class GetAllExpenseCustomAdapter extends RecyclerView.Adapter<GetAllExpenseCustomAdapter.MyViewHolder> {
+    @NonNull
+    @Override
+    public GetAllExpenseCustomAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull GetAllExpenseCustomAdapter.MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
