1b6f5dbbb0415af5c5b189c3f93e6752cda3f1ad
Abdullah272056 <abdullah272056@gmail.com>
2021-01-04 13:31:32 +0600

    create expense_recycler_view_item.xml

--- app/src/main/java/com/example/hostelmealmanagement/ExpensesActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/ExpensesActivity.java
@@ -4,24 +4,38 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class ExpensesActivity extends AppCompatActivity {
 FloatingActionButton addExpenseButton;
+TextView selectMarketerTextView;
+Spinner spinner;
+    String contacts[]={"Ajay","Sachin","Sumit","Tarun","Yogesh"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expenses);
         addExpenseButton=findViewById(R.id.addExpenseButtonId);
+        selectMarketerTextView=findViewById(R.id.selectMarketerTextViewId);
+
         addExpenseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 addCustomerInformation();
             }
         });
+
+
+
+
     }
 
     private void addCustomerInformation(){
@@ -31,6 +45,14 @@ FloatingActionButton addExpenseButton;
         View view                       =layoutInflater.inflate(R.layout.add_expenses,null);
         builder.setView(view);
         final AlertDialog alertDialog   = builder.create();
+
+//        spinner=view.findViewById(R.id.select);
+//
+//        //Creating the ArrayAdapter instance having the country list
+//        ArrayAdapter aa = new ArrayAdapter(this,android.R.layout.simple_spinner_item,contacts);
+//        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        //Setting the ArrayAdapter data on the Spinner
+//        spinner.setAdapter(aa);
 
         alertDialog.show();
     }
