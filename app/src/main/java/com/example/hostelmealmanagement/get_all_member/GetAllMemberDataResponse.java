ebca60b5ce99be5fa1a8a5c5325f1d55488035ba
Abdullah272056 <abdullah272056@gmail.com>
2021-01-04 20:24:24 +0600

    add code in  GetAllMemberDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/get_all_member/GetAllMemberDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/get_all_member/GetAllMemberDataResponse.java
@@ -1,4 +1,31 @@
 package com.example.hostelmealmanagement.get_all_member;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class GetAllMemberDataResponse {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private List<GetAllMemberData> getAllMemberDataList = null;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<GetAllMemberData> getGetAllMemberDataList() {
+        return getAllMemberDataList;
+    }
+
+    public void setGetAllMemberDataList(List<GetAllMemberData> getAllMemberDataList) {
+        this.getAllMemberDataList = getAllMemberDataList;
+    }
 }
