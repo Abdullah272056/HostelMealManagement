9310283123b26b9a653c585f740d3a77dfca2396
Abdullah272056 <abdullah272056@gmail.com>
2021-01-05 19:18:29 +0600

    add code in CreateExpenseGateDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/expense/create_expense/CreateExpenseGateDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/expense/create_expense/CreateExpenseGateDataResponse.java
@@ -1,5 +1,29 @@
 package com.example.hostelmealmanagement.expense.create_expense;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class CreateExpenseGateDataResponse {
-    
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private CreateExpenseGetData createExpenseGetData;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public CreateExpenseGetData getCreateExpenseGetData() {
+        return createExpenseGetData;
+    }
+
+    public void setCreateExpenseGetData(CreateExpenseGetData createExpenseGetData) {
+        this.createExpenseGetData = createExpenseGetData;
+    }
 }
