8fea7b7744864b5e17f9ed40324dfb4f79c6c10c
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 19:09:41 +0600

    create add deposit Method and alert Box show

--- app/src/main/java/com/example/hostelmealmanagement/DepositActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/DepositActivity.java
@@ -1,9 +1,14 @@
 package com.example.hostelmealmanagement;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.hostelmealmanagement.retrofit.ApiInterface;
 import com.example.hostelmealmanagement.retrofit.RetrofitClient;
@@ -13,6 +18,8 @@ public class DepositActivity extends AppCompatActivity {
 FloatingActionButton addDepositFloatingButton;
 ApiInterface apiInterface;
 String token;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,10 +34,25 @@ String token;
         addDepositFloatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addDeposit();
             }
         });
     }
 
-   
+    public  void  addDeposit(){
+        AlertDialog.Builder builder     =new AlertDialog.Builder(DepositActivity.this);
+        LayoutInflater layoutInflater   =LayoutInflater.from(DepositActivity.this);
+        View view                       =layoutInflater.inflate(R.layout.add_deposit,null);
+        builder.setView(view);
+        final AlertDialog alertDialog   = builder.create();
+
+
+
+
+
+
+        alertDialog.show();
+
+
+    }
 }
