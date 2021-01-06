f59f2f40c55e4032c3129401f399d910bf367e84
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 22:05:07 +0600

    receive data

--- app/src/main/java/com/example/hostelmealmanagement/MealActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/MealActivity.java
@@ -9,10 +9,14 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MealActivity extends AppCompatActivity {
     FloatingActionButton addMealFloatingButton;
+    String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal);
+        //receive user token
+        token= getIntent().getStringExtra("token");
+        
         addMealFloatingButton=findViewById(R.id.addMealFloatingButtonId);
         addMealFloatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
