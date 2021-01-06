a44849dd3db12ccdd0c3f13239ba8cee4bde13ca
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 20:11:34 +0600

    add code in AddDepositSetData.java

--- app/src/main/java/com/example/hostelmealmanagement/deposit/AddDepositSetData.java
+++ app/src/main/java/com/example/hostelmealmanagement/deposit/AddDepositSetData.java
@@ -1,4 +1,34 @@
 package com.example.hostelmealmanagement.deposit;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class AddDepositSetData {
+    @SerializedName("depositAmount")
+    @Expose
+    private Integer depositAmount;
+    @SerializedName("user_id")
+    @Expose
+    private String userId;
+
+    public Integer getDepositAmount() {
+        return depositAmount;
+    }
+
+    public void setDepositAmount(Integer depositAmount) {
+        this.depositAmount = depositAmount;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public AddDepositSetData(Integer depositAmount, String userId) {
+        this.depositAmount = depositAmount;
+        this.userId = userId;
+    }
 }
