abe845943aee6809ca9342277e9b061c33a8a229
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 22:02:36 +0600

    view and setOnClickListener in addMealFloatingButton

--- app/src/main/java/com/example/hostelmealmanagement/MealActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/MealActivity.java
@@ -3,12 +3,22 @@ package com.example.hostelmealmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MealActivity extends AppCompatActivity {
-
+    FloatingActionButton addMealFloatingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal);
+        addMealFloatingButton=findViewById(R.id.addMealFloatingButtonId);
+        addMealFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
