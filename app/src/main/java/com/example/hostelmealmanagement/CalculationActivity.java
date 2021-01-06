ead77da6fcca1bc4445f9dd9b9be8f1581c6e3c3
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 10:42:56 +0600

    initialize apiInterface in CalculationActivity.java

--- app/src/main/java/com/example/hostelmealmanagement/CalculationActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/CalculationActivity.java
@@ -4,13 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.hostelmealmanagement.retrofit.ApiInterface;
+import com.example.hostelmealmanagement.retrofit.RetrofitClient;
+
 public class CalculationActivity extends AppCompatActivity {
     String token;
+    ApiInterface apiInterface;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
         //receive user token
         token= getIntent().getStringExtra("token");
+
+        //initialize apiInterface
+        apiInterface = RetrofitClient.getRetrofit("http://hostel-meal-calc.herokuapp.com/").create(ApiInterface.class);
+
     }
 }
