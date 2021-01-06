0629ff267054f927ba29b37a0181127d551b9fb1
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 10:37:38 +0600

    add code in GetAllCalculationDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/calculation/GetAllCalculationDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/calculation/GetAllCalculationDataResponse.java
@@ -1,4 +1,31 @@
 package com.example.hostelmealmanagement.calculation;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class GetAllCalculationDataResponse {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private List<GetAllCalculationData> getAllCalculationDataList = null;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<GetAllCalculationData> getGetAllCalculationDataList() {
+        return getAllCalculationDataList;
+    }
+
+    public void setGetAllCalculationDataList(List<GetAllCalculationData> getAllCalculationDataList) {
+        this.getAllCalculationDataList = getAllCalculationDataList;
+    }
 }
