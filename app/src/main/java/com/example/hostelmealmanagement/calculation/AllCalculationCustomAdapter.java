1f6d8523b313518fa432c087fa935e24a9d5e1f6
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 11:31:46 +0600

    extended Recyclerview in AllCalculationCustomAdapter.java

--- app/src/main/java/com/example/hostelmealmanagement/calculation/AllCalculationCustomAdapter.java
+++ app/src/main/java/com/example/hostelmealmanagement/calculation/AllCalculationCustomAdapter.java
@@ -1,5 +1,32 @@
 package com.example.hostelmealmanagement.calculation;
 
-public class AllCalculationCustomAdapter {
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AllCalculationCustomAdapter extends RecyclerView.Adapter<AllCalculationCustomAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
