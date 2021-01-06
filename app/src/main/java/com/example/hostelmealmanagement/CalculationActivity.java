0370eac3afa0e03eb71852efc3b72108644ae626
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 21:05:51 +0600

    Calculation progressBar function

--- app/src/main/java/com/example/hostelmealmanagement/CalculationActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/CalculationActivity.java
@@ -5,6 +5,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.hostelmealmanagement.calculation.AllCalculationCustomAdapter;
@@ -27,13 +29,17 @@ public class CalculationActivity extends AppCompatActivity {
     List<GetAllCalculationData> getAllCalculationDataList;
     RecyclerView calculationRecyclerView;
     AllCalculationCustomAdapter allCalculationCustomAdapter;
+    ProgressBar calculationProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
+
         //receive user token
         token= getIntent().getStringExtra("token");
+        //view finding
         calculationRecyclerView=findViewById(R.id.calculationRecyclerViewId);
+        calculationProgressBar=findViewById(R.id.calculationProgressBarId);
         //initialize apiInterface
         apiInterface = RetrofitClient.getRetrofit("http://hostel-meal-calc.herokuapp.com/").create(ApiInterface.class);
         finalCalculation();
@@ -53,15 +59,18 @@ public class CalculationActivity extends AppCompatActivity {
                                 CalculationActivity.this,token,getAllCalculationDataList);
                         calculationRecyclerView.setLayoutManager(new LinearLayoutManager(CalculationActivity.this));
                         calculationRecyclerView.setAdapter(allCalculationCustomAdapter);
+
                     }
 
                     Toast.makeText(CalculationActivity.this, "success"+String.valueOf(getAllCalculationDataList.size()), Toast.LENGTH_SHORT).show();
                 }
+                calculationProgressBar.setVisibility(View.INVISIBLE);
             }
 
             @Override
             public void onFailure(Call<GetAllCalculationDataResponse> call, Throwable t) {
                 Toast.makeText(CalculationActivity.this, "fail", Toast.LENGTH_SHORT).show();
+                calculationProgressBar.setVisibility(View.INVISIBLE);
 
             }
         });
