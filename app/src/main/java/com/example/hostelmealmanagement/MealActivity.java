f655298dd180b6d9413fb393e43d8b8dcfbb579f
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 22:18:34 +0600

    create addMeal method and call and dialogBox show

--- app/src/main/java/com/example/hostelmealmanagement/MealActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/MealActivity.java
@@ -1,8 +1,10 @@
 package com.example.hostelmealmanagement;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -21,8 +23,22 @@ public class MealActivity extends AppCompatActivity {
         addMealFloatingButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                addMeal();
             }
         });
+    }
+
+    public void addMeal(){
+        AlertDialog.Builder builder     =new AlertDialog.Builder(MealActivity.this);
+        LayoutInflater layoutInflater   =LayoutInflater.from(MealActivity.this);
+        View view                       =layoutInflater.inflate(R.layout.add_meal,null);
+        builder.setView(view);
+        final AlertDialog alertDialog   = builder.create();
+
+
+
+
+
+        alertDialog.show();
     }
 }
