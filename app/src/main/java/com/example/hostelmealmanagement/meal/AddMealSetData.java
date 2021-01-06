29866a9da0196700654b80868c9d468b6f3f3e66
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 23:22:32 +0600

    write code in AddMealSetData.java

--- app/src/main/java/com/example/hostelmealmanagement/meal/AddMealSetData.java
+++ app/src/main/java/com/example/hostelmealmanagement/meal/AddMealSetData.java
@@ -1,5 +1,30 @@
 package com.example.hostelmealmanagement.meal;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class AddMealSetData {
-    
+    @SerializedName("user_id")
+    @Expose
+    private String userId;
+    @SerializedName("mealCount")
+    @Expose
+    private Integer mealCount;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getMealCount() {
+        return mealCount;
+    }
+
+    public void setMealCount(Integer mealCount) {
+        this.mealCount = mealCount;
+    }
+
 }
