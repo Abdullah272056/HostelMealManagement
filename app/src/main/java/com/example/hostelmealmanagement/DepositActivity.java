7a3a16381b3259abf26cb35756f9a3975616edbb
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 18:48:33 +0600

    receive token

--- app/src/main/java/com/example/hostelmealmanagement/DepositActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/DepositActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.example.hostelmealmanagement.retrofit.ApiInterface;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DepositActivity extends AppCompatActivity {
 FloatingActionButton addDepositFloatingButton;
+ApiInterface apiInterface;
+String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposit);
+        //receive user token
+        token= getIntent().getStringExtra("token");
         //view finding
         addDepositFloatingButton=findViewById(R.id.addDepositFloatingButtonId);
         addDepositFloatingButton.setOnClickListener(new View.OnClickListener() {
