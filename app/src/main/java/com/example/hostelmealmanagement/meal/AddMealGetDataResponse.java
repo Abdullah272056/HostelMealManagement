69f9c62e8eaffbccfa4d4161d4a3e3d84c2a0881
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 23:39:50 +0600

    write code in AddMealGetDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/meal/AddMealGetDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/meal/AddMealGetDataResponse.java
@@ -1,5 +1,29 @@
 package com.example.hostelmealmanagement.meal;
 
-public class AddMealGetDataResponse {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+public class AddMealGetDataResponse {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private AddMealGetBoarderData addMealGetBoarderData;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public AddMealGetBoarderData getAddMealGetBoarderData() {
+        return addMealGetBoarderData;
+    }
+
+    public void setAddMealGetBoarderData(AddMealGetBoarderData addMealGetBoarderData) {
+        this.addMealGetBoarderData = addMealGetBoarderData;
+    }
 }
