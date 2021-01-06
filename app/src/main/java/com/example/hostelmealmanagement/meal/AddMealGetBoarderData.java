3a35971849986dbc1066d1c08e81153c340c683d
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 23:34:18 +0600

    write code in AddMealGetBoarderData.java

--- app/src/main/java/com/example/hostelmealmanagement/meal/AddMealGetBoarderData.java
+++ app/src/main/java/com/example/hostelmealmanagement/meal/AddMealGetBoarderData.java
@@ -1,5 +1,40 @@
 package com.example.hostelmealmanagement.meal;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class AddMealGetBoarderData {
-    
+    @SerializedName("dateAdded")
+    @Expose
+    private String dateAdded;
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("mealCount")
+    @Expose
+    private Integer mealCount;
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getMealCount() {
+        return mealCount;
+    }
+
+    public void setMealCount(Integer mealCount) {
+        this.mealCount = mealCount;
+    }
 }
