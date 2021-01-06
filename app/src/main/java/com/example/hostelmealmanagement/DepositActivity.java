45e3151c46b95d6bf39a7aeefcaee0ce74fcc1a3
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 18:46:52 +0600

    button finding and set listener

--- app/src/main/java/com/example/hostelmealmanagement/DepositActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/DepositActivity.java
@@ -3,12 +3,23 @@ package com.example.hostelmealmanagement;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class DepositActivity extends AppCompatActivity {
-
+FloatingActionButton addDepositFloatingButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deposit);
+        //view finding
+        addDepositFloatingButton=findViewById(R.id.addDepositFloatingButtonId);
+        addDepositFloatingButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
