7a59bf21b47045d69821abe8ef44c7efd2897d99
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 20:18:30 +0600

    write code in AddDepositGetDataResponse.java

--- app/src/main/java/com/example/hostelmealmanagement/deposit/AddDepositGetDataResponse.java
+++ app/src/main/java/com/example/hostelmealmanagement/deposit/AddDepositGetDataResponse.java
@@ -1,5 +1,29 @@
 package com.example.hostelmealmanagement.deposit;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class AddDepositGetDataResponse {
-    
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("data")
+    @Expose
+    private AddDepositGetCustomerData addDepositGetCustomerData;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public AddDepositGetCustomerData getAddDepositGetCustomerData() {
+        return addDepositGetCustomerData;
+    }
+
+    public void setAddDepositGetCustomerData(AddDepositGetCustomerData addDepositGetCustomerData) {
+        this.addDepositGetCustomerData = addDepositGetCustomerData;
+    }
 }
