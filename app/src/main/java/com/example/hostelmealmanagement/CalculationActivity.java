9d6cd8509b6b1f364889d85e66d8f95d88f5a4c7
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 10:32:06 +0600

    receiving token

--- app/src/main/java/com/example/hostelmealmanagement/CalculationActivity.java
+++ app/src/main/java/com/example/hostelmealmanagement/CalculationActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class CalculationActivity extends AppCompatActivity {
-
+    String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculation);
+        //receive user token
+        token= getIntent().getStringExtra("token");
     }
 }
