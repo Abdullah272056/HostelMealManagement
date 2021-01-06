1c1734d535e0c16366bd0326b02ff1b1acf7e5da
Abdullah272056 <abdullah272056@gmail.com>
2021-01-06 11:40:47 +0600

    AllCalculationCustomAdapter.java class's add code

--- app/src/main/java/com/example/hostelmealmanagement/calculation/AllCalculationCustomAdapter.java
+++ app/src/main/java/com/example/hostelmealmanagement/calculation/AllCalculationCustomAdapter.java
@@ -1,12 +1,15 @@
 package com.example.hostelmealmanagement.calculation;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.hostelmealmanagement.R;
 import com.example.hostelmealmanagement.get_all_member.GetAllMemberData;
 import com.example.hostelmealmanagement.get_all_member.MemberListCustomAdapter;
 
@@ -27,22 +30,37 @@ public class AllCalculationCustomAdapter extends RecyclerView.Adapter<AllCalcula
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(context).inflate(R.layout.calculation_recycler_view_item,parent,false);
+        return new AllCalculationCustomAdapter.MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-
+        holder.memberNameTextView.setText(getAllCalculationDataList.get(position).getName());
+        holder.totalMealTextView.setText(String.valueOf(getAllCalculationDataList.get(position).getTotalMeal()));
+        holder.mealRateTextView.setText(String.valueOf(getAllCalculationDataList.get(position).getMealRate()));
+        holder.totalCostTextView.setText(String.valueOf(getAllCalculationDataList.get(position).getTotalCost()));
+        holder.depositAmountTextView.setText(String.valueOf(getAllCalculationDataList.get(position).getDepositAmount()));
+        holder.dueTextView.setText(String.valueOf(getAllCalculationDataList.get(position).getDueOrGetReturn()));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return getAllCalculationDataList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView memberNameTextView,totalMealTextView,mealRateTextView,
+                totalCostTextView,depositAmountTextView,dueTextView;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            memberNameTextView=itemView.findViewById(R.id.memberNameTextViewId);
+            totalMealTextView=itemView.findViewById(R.id.totalMealTextViewId);
+            mealRateTextView=itemView.findViewById(R.id.mealRateTextViewId);
+            totalCostTextView=itemView.findViewById(R.id.totalCostTextViewId);
+            depositAmountTextView=itemView.findViewById(R.id.depositAmountTextViewId);
+            dueTextView=itemView.findViewById(R.id.dueTextViewId);
+
         }
     }
 }
