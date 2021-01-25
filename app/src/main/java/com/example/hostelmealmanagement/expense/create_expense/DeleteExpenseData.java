a2a9349b250225a2b3024b24c7ee7e8756e812e0
Abdullah272056 <abdullah272056@gmail.com>
2021-01-25 10:41:31 +0600

    write code in DeleteExpenseData.java

--- app/src/main/java/com/example/hostelmealmanagement/expense/create_expense/DeleteExpenseData.java
+++ app/src/main/java/com/example/hostelmealmanagement/expense/create_expense/DeleteExpenseData.java
@@ -1,4 +1,96 @@
 package com.example.hostelmealmanagement.expense.create_expense;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class DeleteExpenseData {
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("type")
+    @Expose
+    private String type;
+    @SerializedName("cost")
+    @Expose
+    private Integer cost;
+    @SerializedName("marketer")
+    @Expose
+    private String marketer;
+    @SerializedName("user")
+    @Expose
+    private String user;
+    @SerializedName("dateAdded")
+    @Expose
+    private String dateAdded;
+    @SerializedName("__v")
+    @Expose
+    private Integer v;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Integer getCost() {
+        return cost;
+    }
+
+    public void setCost(Integer cost) {
+        this.cost = cost;
+    }
+
+    public String getMarketer() {
+        return marketer;
+    }
+
+    public void setMarketer(String marketer) {
+        this.marketer = marketer;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(String dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public Integer getV() {
+        return v;
+    }
+
+    public void setV(Integer v) {
+        this.v = v;
+    }
+
 }
